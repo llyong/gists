488d64a9405c81f3d083f43de86236ff8b82e32a
radiomicshub <40889805+llyong@users.noreply.github.com>
2024-03-26 20:09:48 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -142,13 +142,13 @@ elif selected_option=='Comparative Prognostic Accuracy of Proportional versus No
        1.        , 0.        , 1.        , 1.        , 0.        ]], dtype=np.float32)
     
     with open('deepcox.pkl', 'rb') as file:
-    deepcox = pickle.load(file)
-    _ = deepcox.compute_baseline_hazards() #计算基准风险函数
-    surv = deepcox.predict_surv_df(tt) #计算生存函数
-    ax=surv.plot()
-    plt.ylabel('S(t | x)')
-    _ = plt.xlabel('Time')
-    ax.get_legend().remove
+        deepcox = pickle.load(file)
+        _ = deepcox.compute_baseline_hazards() #计算基准风险函数
+        surv = deepcox.predict_surv_df(tt) #计算生存函数
+        ax=surv.plot()
+        plt.ylabel('S(t | x)')
+        _ = plt.xlabel('Time')
+        ax.get_legend().remove
 
 elif selected_option == 'radiomics Comming soon':
 
