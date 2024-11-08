2f71ec74b04920823e6ee4abfe3b6f3373dd3b86
radiomicshub <40889805+llyong@users.noreply.github.com>
2024-11-09 07:27:09 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -26,135 +26,7 @@ options = ['Evaluating Nomogram Models for Predicting Survival Outcomes in Gastr
 selected_option = st.sidebar.radio('Select the Research Paper:', options) #直接选，这个更好
 
 
-
-
 ##########################################################    1    #############################################################
-# 根据选择展示不同的图表
-if selected_option == 'Evaluating Nomogram Models for Predicting Survival Outcomes in Gastric Gastrointestinal Stromal Tumors with SEER Database Analysis':
-    image_file = "seer1.jpg"
-    st.title(":tada: Evaluating nomogram models for predicting survival outcomes in gastric gastrointestinal stromal tumors with SEER database analysis")
-    url = "https://pubmed.ncbi.nlm.nih.gov/38769376/"
-    text_to_display = "DOI: 10.1038/s41598-024-62353-z"
-    st.markdown(f"[{text_to_display}]({url})", unsafe_allow_html=True)
-    
-    # st.image(image_file, caption='GraphicalAbstract', use_column_width=True)
-    st.header("Please enter your information: ")
- 
-    # 类别型
-    Sex = st.selectbox("Sex", options=["Female", "Male"])
-    Race = st.selectbox("Race", options=["Black", "White","Others"])
-    Marital_status_at_diagnosis = st.selectbox("Marital status at diagnosis", options=["Single", "Married"])
-    Tumor_grade = st.selectbox("Tumor grade", options=["Well/moderately differentiated", "Poorly differentiated/undifferentiated"])
-    Tumor_size = st.selectbox("Tumor size", options=["≤2 cm", "2-5cm","5-10cm",">10cm"])
-    AJCC_Stage = st.selectbox("AJCC Stage", options=["Ⅰ","Ⅱ","Ⅲ","Ⅳ"])
-    Surgery = st.selectbox("Surgery", options=["No Surgery", "Local excision","Radical excision"])
-    Regional_nodes_examined = st.selectbox("Regional nodes examined", options=["0", "1-4",">4"])
-    # 数值型
-    Age_at_diagnosis = st.number_input("Age at diagnosis", min_value=20)
-    
-    # 模型输入判决赋值
-    #年龄
-    Age_at_diagnosis = Age_at_diagnosis
-    #性别
-    if Sex == 'Male':
-        Sex_Male = True
-    else:
-        Sex_Male = False
-    # 种族
-    if Race == 'Black':
-        Race_Black = True
-    else:
-        Race_Black = False
-    #
-    if Marital_status_at_diagnosis == "Single":
-        Marital_status_at_diagnosis_Single = True
-    else:
-        Marital_status_at_diagnosis_Single = False
-    #
-    if Tumor_grade == "Poorly differentiated/undifferentiated":
-        Tumor_grade_Poorly_differentiated_undifferentiated = True
-    else:
-        Tumor_grade_Poorly_differentiated_undifferentiated = False
-    #
-    if Tumor_size == "5-10cm":
-        Tumor_size_5_10cm = True
-        Tumor_size_bigger_10cm = False
-    elif Tumor_size == ">10cm":
-        Tumor_size_bigger_10cm = True
-        Tumor_size_5_10cm = False
-    else:
-        Tumor_size_5_10cm = False
-        Tumor_size_bigger_10cm = False
-    #
-    if AJCC_Stage == "Ⅲ":
-        AJCC_Stage_3 = True
-        AJCC_Stage_4 = False
-    elif AJCC_Stage == "Ⅳ":
-        AJCC_Stage_4 = True
-        AJCC_Stage_3 = False
-    else:
-        AJCC_Stage_3 = False
-        AJCC_Stage_4 = False
-    #
-    if Surgery == "No Surgery":
-        Surgery_NoSurgery = True
-    else:
-        Surgery_NoSurgery = False
-    # 这个只有css有
-    if Regional_nodes_examined == ">4":
-        Regional_nodes_examined_bigger_4 = True
-    else:
-        Regional_nodes_examined_bigger_4 = False
-    # 整理输入
-    input_os = {'Sex_Male':Sex_Male,'Race_Black':Race_Black,'Marital_status_at_diagnosis_Single':Marital_status_at_diagnosis_Single,'Tumor_grade_Poorly_differentiated_undifferentiated':Tumor_grade_Poorly_differentiated_undifferentiated,'Tumor_size_5_10cm':Tumor_size_5_10cm,'Tumor_size_bigger_10cm':Tumor_size_bigger_10cm,'AJCC_Stage_3':AJCC_Stage_3,'AJCC_Stage_4':AJCC_Stage_4,'Surgery_NoSurgery':Surgery_NoSurgery,'Age_at_diagnosis':Age_at_diagnosis}
-    input_css = {'Sex_Male':Sex_Male,'Race_Black':Race_Black,'Marital_status_at_diagnosis_Single':Marital_status_at_diagnosis_Single,'Tumor_grade_Poorly_differentiated_undifferentiated':Tumor_grade_Poorly_differentiated_undifferentiated,'Tumor_size_5_10cm':Tumor_size_5_10cm,'Tumor_size_bigger_10cm':Tumor_size_bigger_10cm,'AJCC_Stage_3':AJCC_Stage_3,'AJCC_Stage_4':AJCC_Stage_4,'Surgery_NoSurgery':Surgery_NoSurgery,'Regional_nodes_examined_bigger_4':Regional_nodes_examined_bigger_4,'Age_at_diagnosis':Age_at_diagnosis}
-    #os_df = pd.DataFrame(input_os)
-    #css_df = pd.DataFrame(input_css)
-    os_df = pd.Series(input_os)
-    css_df = pd.Series(input_css)
-
-    # 导入模型
-    with open('cph_os.pkl', 'rb') as file:
-        cph_os = pickle.load(file)
-        os_half = cph_os.predict_survival_function(os_df).loc[6]
-        os_1 = cph_os.predict_survival_function(os_df).loc[12]
-        os_3 = cph_os.predict_survival_function(os_df).loc[36]
-        os_5 = cph_os.predict_survival_function(os_df).loc[60]
-        os_7 = cph_os.predict_survival_function(os_df).loc[84]
-        os_10 = cph_os.predict_survival_function(os_df).loc[120]
-    
-    with open('cph_css.pkl', 'rb') as file:
-        cph_css = pickle.load(file)
-        css_half = cph_css.predict_survival_function(css_df).loc[6]
-        css_1 = cph_css.predict_survival_function(css_df).loc[12]
-        css_3 = cph_css.predict_survival_function(css_df).loc[36]
-        css_5 = cph_css.predict_survival_function(css_df).loc[60]
-        css_7 = cph_css.predict_survival_function(css_df).loc[84]
-        css_10 = cph_css.predict_survival_function(css_df).loc[120]
-
-    #输出结果
-    
-    st.subheader("OS cox nomograms at six-time points: ")
-    st.write(f"Half a Year ： {str(os_half.values)}")
-    st.write(f"One Year ： {os_1.values}")
-    st.write(f"Three Years ： {os_3.values}")
-    st.write(f"Five Years ： {os_5.values}")
-    st.write(f"Seven Years ： {os_7.values}")
-    st.write(f"Ten Years ： {os_10.values}")
-
-    st.subheader("CSS cox nomograms at six-time points: ")
-    st.write(f"Half a Year ： {css_half.values}")
-    st.write(f"One Year ： {css_1.values}")
-    st.write(f"Three Years ： {css_3.values}")
-    st.write(f"Five Years ： {css_5.values}")
-    st.write(f"Seven Years ： {css_7.values}")
-    st.write(f"Ten Years ： {css_10.values}")
-    st.markdown('''
-    **Disclaimer:**
-    This research is currently in the laboratory phase. The findings and outcomes presented are preliminary and have not been subjected to the rigorous testing and validation required for clinical application. The use of the information, techniques, or products described herein is at the user's own risk. It is imperative that any potential clinical application be preceded by thorough scientific evaluation and regulatory approval. The authors and affiliated institutions assume no liability for any adverse consequences resulting from the use of the information provided.
-    ''')
-
-##########################################################    2    #############################################################
 
 elif selected_option=='Integrating MissCatBoosts Imputation with SEER Data to Compare Proportional and Non-Proportional Hazards Models in Gastric GIST Prognosis':
     # 这里分三页，但注意里面的参数是算作一页的
@@ -1178,7 +1050,132 @@ with open('deepcox.pkl', 'rb') as file:
     ax.get_legend().remove''',language='python',line_numbers=True)
         st.code('''''',language='python',line_numbers=True)
 
-        
+##########################################################    2    #############################################################
+# 根据选择展示不同的图表
+if selected_option == 'Evaluating Nomogram Models for Predicting Survival Outcomes in Gastric Gastrointestinal Stromal Tumors with SEER Database Analysis':
+    image_file = "seer1.jpg"
+    st.title(":tada: Evaluating nomogram models for predicting survival outcomes in gastric gastrointestinal stromal tumors with SEER database analysis")
+    url = "https://pubmed.ncbi.nlm.nih.gov/38769376/"
+    text_to_display = "DOI: 10.1038/s41598-024-62353-z"
+    st.markdown(f"[{text_to_display}]({url})", unsafe_allow_html=True)
+    
+    # st.image(image_file, caption='GraphicalAbstract', use_column_width=True)
+    st.header("Please enter your information: ")
+ 
+    # 类别型
+    Sex = st.selectbox("Sex", options=["Female", "Male"])
+    Race = st.selectbox("Race", options=["Black", "White","Others"])
+    Marital_status_at_diagnosis = st.selectbox("Marital status at diagnosis", options=["Single", "Married"])
+    Tumor_grade = st.selectbox("Tumor grade", options=["Well/moderately differentiated", "Poorly differentiated/undifferentiated"])
+    Tumor_size = st.selectbox("Tumor size", options=["≤2 cm", "2-5cm","5-10cm",">10cm"])
+    AJCC_Stage = st.selectbox("AJCC Stage", options=["Ⅰ","Ⅱ","Ⅲ","Ⅳ"])
+    Surgery = st.selectbox("Surgery", options=["No Surgery", "Local excision","Radical excision"])
+    Regional_nodes_examined = st.selectbox("Regional nodes examined", options=["0", "1-4",">4"])
+    # 数值型
+    Age_at_diagnosis = st.number_input("Age at diagnosis", min_value=20)
+    
+    # 模型输入判决赋值
+    #年龄
+    Age_at_diagnosis = Age_at_diagnosis
+    #性别
+    if Sex == 'Male':
+        Sex_Male = True
+    else:
+        Sex_Male = False
+    # 种族
+    if Race == 'Black':
+        Race_Black = True
+    else:
+        Race_Black = False
+    #
+    if Marital_status_at_diagnosis == "Single":
+        Marital_status_at_diagnosis_Single = True
+    else:
+        Marital_status_at_diagnosis_Single = False
+    #
+    if Tumor_grade == "Poorly differentiated/undifferentiated":
+        Tumor_grade_Poorly_differentiated_undifferentiated = True
+    else:
+        Tumor_grade_Poorly_differentiated_undifferentiated = False
+    #
+    if Tumor_size == "5-10cm":
+        Tumor_size_5_10cm = True
+        Tumor_size_bigger_10cm = False
+    elif Tumor_size == ">10cm":
+        Tumor_size_bigger_10cm = True
+        Tumor_size_5_10cm = False
+    else:
+        Tumor_size_5_10cm = False
+        Tumor_size_bigger_10cm = False
+    #
+    if AJCC_Stage == "Ⅲ":
+        AJCC_Stage_3 = True
+        AJCC_Stage_4 = False
+    elif AJCC_Stage == "Ⅳ":
+        AJCC_Stage_4 = True
+        AJCC_Stage_3 = False
+    else:
+        AJCC_Stage_3 = False
+        AJCC_Stage_4 = False
+    #
+    if Surgery == "No Surgery":
+        Surgery_NoSurgery = True
+    else:
+        Surgery_NoSurgery = False
+    # 这个只有css有
+    if Regional_nodes_examined == ">4":
+        Regional_nodes_examined_bigger_4 = True
+    else:
+        Regional_nodes_examined_bigger_4 = False
+    # 整理输入
+    input_os = {'Sex_Male':Sex_Male,'Race_Black':Race_Black,'Marital_status_at_diagnosis_Single':Marital_status_at_diagnosis_Single,'Tumor_grade_Poorly_differentiated_undifferentiated':Tumor_grade_Poorly_differentiated_undifferentiated,'Tumor_size_5_10cm':Tumor_size_5_10cm,'Tumor_size_bigger_10cm':Tumor_size_bigger_10cm,'AJCC_Stage_3':AJCC_Stage_3,'AJCC_Stage_4':AJCC_Stage_4,'Surgery_NoSurgery':Surgery_NoSurgery,'Age_at_diagnosis':Age_at_diagnosis}
+    input_css = {'Sex_Male':Sex_Male,'Race_Black':Race_Black,'Marital_status_at_diagnosis_Single':Marital_status_at_diagnosis_Single,'Tumor_grade_Poorly_differentiated_undifferentiated':Tumor_grade_Poorly_differentiated_undifferentiated,'Tumor_size_5_10cm':Tumor_size_5_10cm,'Tumor_size_bigger_10cm':Tumor_size_bigger_10cm,'AJCC_Stage_3':AJCC_Stage_3,'AJCC_Stage_4':AJCC_Stage_4,'Surgery_NoSurgery':Surgery_NoSurgery,'Regional_nodes_examined_bigger_4':Regional_nodes_examined_bigger_4,'Age_at_diagnosis':Age_at_diagnosis}
+    #os_df = pd.DataFrame(input_os)
+    #css_df = pd.DataFrame(input_css)
+    os_df = pd.Series(input_os)
+    css_df = pd.Series(input_css)
+
+    # 导入模型
+    with open('cph_os.pkl', 'rb') as file:
+        cph_os = pickle.load(file)
+        os_half = cph_os.predict_survival_function(os_df).loc[6]
+        os_1 = cph_os.predict_survival_function(os_df).loc[12]
+        os_3 = cph_os.predict_survival_function(os_df).loc[36]
+        os_5 = cph_os.predict_survival_function(os_df).loc[60]
+        os_7 = cph_os.predict_survival_function(os_df).loc[84]
+        os_10 = cph_os.predict_survival_function(os_df).loc[120]
+    
+    with open('cph_css.pkl', 'rb') as file:
+        cph_css = pickle.load(file)
+        css_half = cph_css.predict_survival_function(css_df).loc[6]
+        css_1 = cph_css.predict_survival_function(css_df).loc[12]
+        css_3 = cph_css.predict_survival_function(css_df).loc[36]
+        css_5 = cph_css.predict_survival_function(css_df).loc[60]
+        css_7 = cph_css.predict_survival_function(css_df).loc[84]
+        css_10 = cph_css.predict_survival_function(css_df).loc[120]
+
+    #输出结果
+    
+    st.subheader("OS cox nomograms at six-time points: ")
+    st.write(f"Half a Year ： {str(os_half.values)}")
+    st.write(f"One Year ： {os_1.values}")
+    st.write(f"Three Years ： {os_3.values}")
+    st.write(f"Five Years ： {os_5.values}")
+    st.write(f"Seven Years ： {os_7.values}")
+    st.write(f"Ten Years ： {os_10.values}")
+
+    st.subheader("CSS cox nomograms at six-time points: ")
+    st.write(f"Half a Year ： {css_half.values}")
+    st.write(f"One Year ： {css_1.values}")
+    st.write(f"Three Years ： {css_3.values}")
+    st.write(f"Five Years ： {css_5.values}")
+    st.write(f"Seven Years ： {css_7.values}")
+    st.write(f"Ten Years ： {css_10.values}")
+    st.markdown('''
+    **Disclaimer:**
+    This research is currently in the laboratory phase. The findings and outcomes presented are preliminary and have not been subjected to the rigorous testing and validation required for clinical application. The use of the information, techniques, or products described herein is at the user's own risk. It is imperative that any potential clinical application be preceded by thorough scientific evaluation and regulatory approval. The authors and affiliated institutions assume no liability for any adverse consequences resulting from the use of the information provided.
+    ''')
+
 
 ##########################################################    3    #############################################################
 
